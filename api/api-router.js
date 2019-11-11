f49d98a3b270142f67248ab25241d721ab028b85
abarne <celius377@gmail.com>
2019-11-11 11:32:29 -0600

    hash the password

--- api/api-router.js
+++ api/api-router.js
@@ -6,6 +6,8 @@ const usersRouter = require('../users/users-router.js');
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
+let bcrypt = require('bcryptjs');
+
 router.get('/', (req, res) => {
 	res.json({ api: "It's alive" });
 });
@@ -15,6 +17,11 @@ router.post('/hash', (req, res) => {
 	// hash the password using bcryptjs
 	// return it to the user in an object that looks like
 	// { password: 'original passsword', hash: 'hashed password' }
+	const original = req.body.password;
+	const credentials = req.body;
+	const hash = bcrypt.hashSync(credentials.password, 14);
+	credentials.password = hash;
+	res.json({ originalPassword: original, hashedPassword: hash });
 });
 
 module.exports = router;
